585c772c0737a29c183117c2c57c6187bbd5b50b
Artan Thaqi <artaan.thaqi@gmail.com>
2023-11-16 10:18:37 +0100

    Sockets added for the service to run and log the data sent to it

--- Service/src/main/java/org/example/Main.java
+++ Service/src/main/java/org/example/Main.java
@@ -1,26 +1,65 @@
 package org.example;
 
+import com.sun.jna.Native;
+import com.sun.jna.platform.win32.Advapi32;
+import com.sun.jna.platform.win32.WinNT;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
     
-public static void main(String[] args) {
-        // Log an event
-        logEvent("Application", "EventViewerLogger", "Sample log message", WinNT.EVENTLOG_INFORMATION_TYPE);
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(12345);  // Listen on port 12345
+            System.out.println("Service is running. Waiting for data...");
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+                handleClientRequest(socket);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private static void logEvent(String logName, String sourceName, String message, int eventType) {
-        WinNT.HANDLE hEventLog = Advapi32.INSTANCE.RegisterEventSource(null, logName);
+    private static void handleClientRequest(Socket socket) {
+        try {
+            InputStream inputStream = socket.getInputStream();
+
+            byte[] buffer = new byte[1024];
+            int bytesRead = inputStream.read(buffer);
+
+            if (bytesRead > 0) {
+                String receivedData = new String(buffer, 0, bytesRead);
+                logDataToEventViewer(receivedData);
+                System.out.println("Received data: " + receivedData);
+            }
+
+            inputStream.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void logDataToEventViewer(String data) {
+        WinNT.HANDLE hEventLog = Advapi32.INSTANCE.RegisterEventSource(null, "Application");
 
         if (hEventLog != null) {
             // Log the event
             boolean success = Advapi32.INSTANCE.ReportEvent(
                     hEventLog,
-                    eventType,
+                    WinNT.EVENTLOG_INFORMATION_TYPE,
                     0,
                     0,
                     null,
                     1,
                     0,
-                    new String[]{message},
+                    new String[]{data},
                     null
             );
 
